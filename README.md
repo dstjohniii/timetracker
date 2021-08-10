b8ec04290cd98c1351c69ac078a73dd00ee3108f
Denis St. John <dstjohniii@outlook.com>
2021-08-09 19:10:37 -0500

    fea: adds database compose things.

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ docker exec -it ttdb bash
 su postgres
 psql
 \conninfo
+
+## Magical database Postgres connection
+docker-compose run database bash
+psql --host=database --username=unicorn_user --dbname=rainbow_database
+magical_password
+
+## Testing
+http://localhost:9002/graphql
--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express')
-require('dotenv').config({
-  path: '../.env'
-})
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+require("dotenv").config({
+  path: "../.env",
+});
 
 const port = process.env.PORT;
 
@@ -12,24 +12,26 @@ async function startApolloServer() {
     type Query {
       hello: String
     }
-  `
+  `;
 
   // Provide resolver functions for your schema fields
   const resolvers = {
     Query: {
-      hello: () => 'Hello world!',
+      hello: () => "Hello world!",
     },
-  }
+  };
 
-  const server = new ApolloServer({ typeDefs, resolvers })
-  await server.start()
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
 
-  const app = express()
-  server.applyMiddleware({ app })
+  const app = express();
+  server.applyMiddleware({ app });
 
-  await new Promise((resolve) => app.listen({port}, resolve))
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
-  return { server, app }
+  await new Promise((resolve) => app.listen({ port }, resolve));
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+  return { server, app };
 }
 
 startApolloServer();
